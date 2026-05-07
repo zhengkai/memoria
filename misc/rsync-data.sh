ebc8749bef0191a055cd18f7616a4948086ea5d2
Zheng Kai <zhengkai@gmail.com>
2026-05-07 10:19:48 +0800

    fix: rsync script

--- misc/rsync-data.sh
+++ misc/rsync-data.sh
@@ -1,26 +1,30 @@
 #! /usr/bin/env bash
 
+set -e
+
+TARGET="doll:/www/memoria/static"
+
 # 将 tanya 上的正式数据同步到 doll 上
 #
 # 应该 crontab 每分钟执行
 
-cd "$(dirname "$(readlink -f "$0")")" || exit 1
-cd ../static || exit 1
+cd "$(dirname "$(dirname "$(readlink -f "$0")")")/static"
 
 LOCKFILE=".$(basename "$0").lock"
 exec 200>"$LOCKFILE"
-flock -n 200 || exit 1
+flock -n 200
 
 RSYNC_TIME_FILE="rsync-time.txt"
+EXPORT_TIME_FILE="data/export-time.txt"
 
 PREV="$(cat "$RSYNC_TIME_FILE" 2>/dev/null || echo 0)"
-
-TS="$(cat export-time.txt 2>/dev/null || echo 0)"
-
-if [[ "$TS" == "$PREV" ]]; then
+TS="$(cat "$EXPORT_TIME_FILE" 2>/dev/null || echo 0)"
+if [[ "x$TS" == "$PREV" ]]; then
 	exit
 fi
 
-rsync --partial -vzrtopg --delete ./data ./file doll:/www/memoria || exit 1
+set -x
+rsync --partial -vzrtopg --delete --exclude="$EXPORT_TIME_FILE" ./data ./file "$TARGET"
+rsync --partial -vzrtopg "$EXPORT_TIME_FILE" "${TARGET}/${EXPORT_TIME_FILE}"
 
 echo "$TS" > "$RSYNC_TIME_FILE"
