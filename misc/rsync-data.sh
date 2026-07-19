4c183746e8173b009ec479a8d8ac6e7e4ab857c6
Zheng Kai <zhengkai@gmail.com>
2026-07-19 20:48:08 +0800

    chore: script

--- misc/rsync-data.sh
+++ misc/rsync-data.sh
@@ -15,7 +15,7 @@ exec 200>"$LOCKFILE"
 flock -n 200
 
 RSYNC_TIME_FILE="rsync-time.txt"
-EXPORT_TIME_FILE="data/export-time.txt"
+EXPORT_TIME_FILE="data-v2/export-time.txt"
 
 PREV="$(cat "$RSYNC_TIME_FILE" 2>/dev/null || echo 0)"
 TS="$(cat "$EXPORT_TIME_FILE" 2>/dev/null || echo 0)"
@@ -24,7 +24,7 @@ if [[ "x$TS" == "$PREV" ]]; then
 fi
 
 set -x
-rsync --partial -vzrtopg --delete --xattrs --exclude="$EXPORT_TIME_FILE" ./data ./file "$TARGET"
+rsync --partial -vzrtopg --delete --xattrs --exclude="$EXPORT_TIME_FILE" ./data-v2 ./file "$TARGET"
 rsync --partial -vzrtopg "$EXPORT_TIME_FILE" "${TARGET}/${EXPORT_TIME_FILE}"
 
 echo "$TS" > "$RSYNC_TIME_FILE"
